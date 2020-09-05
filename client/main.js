1164984cc46488bdf54552e9131f68ecb4018c5a
AdrianSocasToledo <adrian.socastoledo@gmail.com>
2020-09-05 15:44:41 +0100

    Eliminando cosas

--- client/main.js
+++ client/main.js
@@ -1,35 +1,34 @@
-var socket = io.connect('',{'forceNew':true}); //aqui va la IP del server al que conectarse.
+var socket = io.connect("http://localhost:6677", { forceNew: true }); //aqui va la IP del server al que conectarse.
 
-socket.on('messages',function(data){
-    console.log(data);
-    render(data);
+socket.on("messages", function (data) {
+  console.log(data);
+  render(data);
 });
 
-function render(data){
-    var html = data.map(function(message,index){
-        return (`
+function render(data) {
+  var html = data
+    .map(function (message, index) {
+      return `
             <div class="message">
                 <strong>${message.nickname}</strong> : 
                 <span>${message.text}</span>
             </div>
-        `);
-    }).join(' ');
+        `;
+    })
+    .join(" ");
 
-    var div_messages = document.getElementById('messages');
-    div_messages.innerHTML = html;
-    div_messages.scrollTop = div_messages.scrollHeight;
-
-
+  var div_messages = document.getElementById("messages");
+  div_messages.innerHTML = html;
+  div_messages.scrollTop = div_messages.scrollHeight;
 }
 
-function addMessage(e){
-    var message = {
-        nickname: document.getElementById("nickname").value,
-        text: document.getElementById("text").value
-    };
+function addMessage(e) {
+  var message = {
+    nickname: document.getElementById("nickname").value,
+    text: document.getElementById("text").value,
+  };
 
-    document.getElementById("nickname").style.display = "none";
-    socket.emit('add-message',message);
-    return false;
-
+  document.getElementById("nickname").style.display = "none";
+  socket.emit("add-message", message);
+  return false;
 }
